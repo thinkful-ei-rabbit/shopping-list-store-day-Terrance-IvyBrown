dfea88d7d356f50c1d783f6e81c85bb7832f1110
TDest1n <boxman06@yahoo.com>
2020-08-13 11:29:10 -0500

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const store = {
   items: [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
+    { id: cuid(), name: 'apples', checked: false }, 
+    { id: cuid(), name: 'oranges', checked: true }, 
+    {...},
+    {...}
+    // and so on...
   ],
-  hideCheckedItems: false
 };
 
 const generateItemElement = function (item) {
